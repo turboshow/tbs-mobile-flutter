80b06016fab6eafc024494819c9f81266f87b29d
Dong Han <dong.han@points.org>
2019-07-30 00:35:24 +0800

    fullscreen landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tbs/ui/playlist_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
